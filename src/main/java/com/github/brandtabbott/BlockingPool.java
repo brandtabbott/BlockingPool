03f29fa7a12c4fb44ab45cf4ffbe6d5245cc1cc7
Brandt Abbott <brandt.abbott@readytalk.com>
2015-10-12 10:56:45 -0600

    Initial commit

---
+++ src/main/java/com/github/brandtabbott/BlockingPool.java
@@ -0,0 +1,51 @@
+package com.ecovate.provision.l3;
+
+import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BlockingPool<E> {
+  private final Logger logger = LoggerFactory.getLogger(BlockingPool.class);
+  
+  private final ConcurrentLinkedQueue<E> queue;
+  
+   BlockingPool(Collection<E> poolContent) throws BlockingPoolException{
+    if(poolContent == null || poolContent.size() < 1)
+      throw new BlockingPoolException("Cannot initialize a BlockingPool without pool content");
+    
+    queue = new ConcurrentLinkedQueue<E>(poolContent);    
+  }
+   
+  public E borrow(){
+    logger.debug("Attempting to borrow from BlockingPool with size: "+queue.size());
+    if(queue.peek() == null){
+      try{
+        synchronized(this){
+          logger.info("BlockingPool contains "+queue.size()+" elements, queue.peek is null, waiting for notification...");
+          wait();
+        }                  
+      }
+      catch(InterruptedException e){
+        logger.debug("BlockingPool thread interrupted!!");
+        e.printStackTrace();
+      }
+    }
+    
+    logger.debug("Borrowed one item from the BlockingPool with size "+queue.size());
+    return queue.poll();
+  }
+  
+  public void release(E borrowed){
+    logger.debug("Releasing one item back into the BlockingPool with size "+queue.size());
+    queue.offer(borrowed);
+    synchronized(this){
+      notify();
+    }    
+  }
+  
+  public int size(){
+    return queue.size();
+  }
+}
---
+++ src/main/java/com/github/brandtabbott/BlockingPoolException.java
@@ -0,0 +1,27 @@
+package com.ecovate.provision.l3;
+
+/* Caught exception for provisioning */
+public class BlockingPoolException extends RuntimeException {
+
+  private static final long serialVersionUID = 6589942387653952899L;
+
+  public BlockingPoolException() {
+    super();
+  }
+
+  public BlockingPoolException(String message) {
+    super(message);
+  }
+
+  public BlockingPoolException(Throwable cause) {
+    super(cause);
+  }
+
+  public BlockingPoolException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public BlockingPoolException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+}
