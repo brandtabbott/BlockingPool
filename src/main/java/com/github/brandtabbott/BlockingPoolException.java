621e3ac9b19a8bff665d66d1068c78b1084ee9b6
Brandt Abbott <brandt.abbott@readytalk.com>
2015-10-28 13:15:14 -0600

    Removed unecessary comment

--- src/main/java/com/github/brandtabbott/BlockingPoolException.java
+++ src/main/java/com/github/brandtabbott/BlockingPoolException.java
@@ -1,6 +1,5 @@
 package com.github.brandtabbott;
 
-/* Caught exception for provisioning */
 public class BlockingPoolException extends RuntimeException {
 
   private static final long serialVersionUID = 6589942387653952899L;
